d8ca8efeae7d750a28cad57176db6d109e04ea02
thumptraining <thumptraining@gmail.com>
2021-12-14 11:49:03 -0300

    subida

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -21,7 +21,7 @@
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body>
+    <body class="overflow-hidden">
         <div class="font-sans text-gray-900 antialiased overflow-hidden">
             {{ $slot }}
         </div>
