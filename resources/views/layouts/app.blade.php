51461de7e843e0cbf3b2d104ba9516639a56a473
thumptraining <thumptraining@gmail.com>
2021-12-17 18:49:54 -0300

    menu creacion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -102,7 +102,7 @@
             <!-- CERRAR -->
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
-                <button href="{{ route('logout') }}" class="flex flex-col items-center transition ease-in duration-200 hover:text-red-400 " onclick="event.preventDefault(); this.closest('form').submit();">
+                <button href="{{ route('logout') }}" class="flex flex-col items-center transition ease-in duration-200  text-red-300 hover:text-red-400 " onclick="event.preventDefault(); this.closest('form').submit();">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                     </svg>
